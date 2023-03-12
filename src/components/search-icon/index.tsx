e6dabad5e4bf3ed79db13312800c819042b0b801
alejandra-leal <alemayagoitia@outlook.com>
2023-03-12 09:17:04 +0100

    Fixing svg attributes

--- src/components/search-icon/index.tsx
+++ src/components/search-icon/index.tsx
@@ -16,14 +16,14 @@ export const SearchIcon: React.FC<IProps> = ({ color }) => {
       <path
         d="M17 17L21 21"
         stroke={color}
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z"
         stroke={color}
-        stroke-width="2"
+        strokeWidth="2"
       />
     </svg>
   );
